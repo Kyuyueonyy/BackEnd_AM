005d91efb74867745c309c64a7a367dbed70973b
kyuyueonyy <rbddl3470@naver.com>
2025-06-10 14:20:22 +0900

    SpringLegacy 템플릿 생성

---
+++ Spring2/test/src/main/java/org/scoula/Chef.java
@@ -0,0 +1,8 @@
+package org.scoula;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //싱글톤 객체 생성
+public class Chef {
+}
